acf389b6781ad1e4a3208419c6fc261e5b9f0b75
denis <denislegrace@yandex.ru>
2022-06-28 10:38:54 +0500

    some changes

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import sqlite3
 from typing import List, Union
 
 # TODO: добавить имя базы в переменную виртуального окружения
-DATABASE_NAME = 'db/basketbot.db'
+DATABASE_NAME = 'basketbot.db'
 
 CREATE_DATA_TABLE = """
     CREATE TABLE IF NOT EXISTS 'cashdata' (
