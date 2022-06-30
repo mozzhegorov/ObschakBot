fa0fcdf6c80a783f2e468ac41738218de18152a6
denis <denislegrace@yandex.ru>
2022-06-30 12:53:16 +0500

    some changes

--- db.py
+++ db.py
@@ -114,7 +114,7 @@ SELECT calc_id, calc_alias FROM cashdata
 WHERE user_id=%s
 """
 LAST_CALC_DATA = """
-SELECT calc_id, calc_alias FROM 'calc_now'
+SELECT *ss FROM calc_now
 WHERE user_id=%s;
 """
 INSERT_CALC = """
