773f82ffdb5ed448ef8098f369b6061ef4d36d39
denis <denislegrace@yandex.ru>
2022-06-30 16:31:53 +0500

    some changes

--- services.py
+++ services.py
@@ -38,7 +38,7 @@ def add_peceipt(message: str, user_id: int):
 def add_new_calc(alias: str, user_id: int):
     calculations_data = data_base_fetchone(GET_CALC_BY_USER, (user_id,))
     if calculations_data:
-        data_base_action(UPDATE_CALC, (calculations_data[0] + 1, alias, user_id))
+        data_base_action(UPDATE_CALC, (calculations_data[0]+1, alias, user_id))
     else:
         data_base_action(INSERT_CALC, (user_id, 1, alias))
 
@@ -69,27 +69,25 @@ def change_calc(user_id: int, calc_id: int):
 def get_dict_of_credits_data(user_id: int):
     calculations_data = data_base_fetch(LAST_CALC_DATA, (user_id,))
     last_calc_id = calculations_data[0][0] if calculations_data else 0
-    all_receipts = data_base_fetch(GET_ALL_RECEIPT_DATA, (user_id, last_calc_id))
+    get_all_receipts = data_base_fetch(GET_ALL_RECEIPT_DATA, (user_id, last_calc_id))
     result_dict = {}
     alias = str
-    for receipt in all_receipts:
-        sponsor = receipt[5]
-        consumers = [consumer for consumer in receipt[7:25] if consumer is not None]
-        if sponsor not in result_dict:
-            result_dict[sponsor] = {}
-        for consumer in consumers:
-            if consumer not in result_dict[sponsor]:
-                result_dict[sponsor][consumer] = 0
-
-    for receipt in all_receipts:
+    for receipt in get_all_receipts:
         alias = receipt[3]
         sponsor = receipt[5]
         money = receipt[6]
         consumers = [consumer for consumer in receipt[7:25] if consumer is not None]
+        if sponsor not in result_dict:
+            result_dict[sponsor] = {}
         for consumer in consumers:
-            dif_money = result_dict[consumer][sponsor] - money / len(consumers)
-            result_dict[consumer][sponsor] += dif_money
-            result_dict[sponsor][consumer] -= dif_money
+            if consumer == sponsor:
+                continue
+            if consumer not in result_dict[sponsor]:
+                result_dict[sponsor][consumer] = 0
+            if consumer in result_dict and sponsor in result_dict[consumer]:
+                result_dict[consumer][sponsor] -= money / len(consumers)
+            else:
+                result_dict[sponsor][consumer] += money / len(consumers)
     return last_calc_id, alias, result_dict
 
 
