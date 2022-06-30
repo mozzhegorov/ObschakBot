362a34a8ced5db26426075b3caba86af46b2d6c1
denis <denislegrace@yandex.ru>
2022-06-30 11:20:59 +0500

    some changes

--- db.py
+++ db.py
@@ -210,7 +210,11 @@ def data_base_fetch(script, inserted_data=None):
 def data_base_fetchone(script, inserted_data=None):
     with dbdriver.connect(DATABASE_NAME) as conn:
         cursor = conn.cursor()
-        return cursor.execute(script, inserted_data).fetchone()
+        exec_result = cursor.execute(script, inserted_data)
+        if exec_result:
+            return cursor.execute(script, inserted_data).fetchone()
+        else:
+            return None
 
 
 if __name__ == '__main__':
