38c9fb5aa095f0ee5eac2ecd0e03216826888e40
denis <denislegrace@yandex.ru>
2022-07-04 12:36:47 +0500

    debug

--- texts.py
+++ texts.py
@@ -45,12 +45,12 @@ def text_all_calcs(all_calcs):
 def text_all_receipts(all_receipts):
     calc_id = all_receipts[0][1]
     calc_alias = all_receipts[0][2]
-    calc_alias = calc_alias if calc_alias else bold('(Без имени)')
+    calc_alias = calc_alias if calc_alias else '(Без имени)'
     answer = bold(f'Список всех чеков по расчету номер {calc_id} с именем {calc_alias} \n')
     for receipt in all_receipts:
         user_id, calc_id, calc_alias, receipt_num, sponsor, receipt_money, *consumers = receipt
         consumers = [consumer for consumer in consumers if consumer is not None]
-        answer += (bold(f'{receipt_num} : ') +
+        answer += (bold(f'{receipt_num}: ') +
                    bold(f'{sponsor}') + ' оплатил ' + italic(f'{receipt_money} ') +
                    f'за {consumers} \n')
     return text(answer)
