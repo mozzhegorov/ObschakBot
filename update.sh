980596a5f51c2271fabf01cc1e861ace01d68699
denis <denislegrace@yandex.ru>
2022-08-05 10:54:18 +0500

    edit print reports

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 git pull origin
-docker build --network=host -t obimage ./
+docker build --network=host -t obschakim ./
 docker stop obschak
 docker rm obschak
-docker run --name obschak -d --restart unless-stopped -v ${PWD}:/home -p 8081:8081 obimage
+docker run --name obschak -d --restart unless-stopped -v ${PWD}:/home -p 8081:8081 obschakim
 
