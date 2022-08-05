266bcec9a7eab888526afcd3b339395f8e49520e
denis <denislegrace@yandex.ru>
2022-08-05 11:01:16 +0500

    edit print reports

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@ git pull origin
 docker build --network=host -t obschakim ./
 docker stop obschak
 docker rm obschak
-docker run --name obschak -d --restart unless-stopped -v ${PWD}:/home -p 8081:8081 obschakim
+docker run --name obschak -d --restart unless-stopped -v ${PWD}:/home obschakim
 
