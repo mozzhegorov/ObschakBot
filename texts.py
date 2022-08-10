c2259feb01fe23af81e2a30d034aded02549b054
denis <denislegrace@yandex.ru>
2022-08-10 16:50:06 +0500

    refactoring

--- texts.py
+++ texts.py
@@ -1,22 +1,22 @@
-from aiogram.utils.markdown import code, text, italic, bold
+from aiogram.utils.markdown import text
 
-START_TEXT = text(bold('Общак бот \n'),
+START_TEXT = text('*Общак бот \n*',
                   'Расчет долгов сложных трат при оплате одним человеком \n',
                   'Список возможных команд: \n',
-                  italic('/new'), ' - Новый расчет \n',
-                  italic('Имя1 300 Имя2 Имя3'), ' - Формат ввода данных чека. Имя1 - оплатил чек, '
-                                                '300 - сумма чека, Имя2 и Имя3 - должники  \n',
-                  italic('/report'), ' - Вывод текущего расчета \n',
-                  italic('/picreport'), ' - Вывод текущего расчета в виде  \n',
-                  italic('/report Имя1 Имя2'), ' - Вывод детали расчета. '
-                                               'Можно узнать сколько Имя2 должен Имя2 \n',
-                  italic('/all'), ' - Вывод всех расчетов \n',
-                  italic('/del1'), ' - Удаление расчета под номером 1 \n',
-                  italic('/calc1'), ' - Сделать активным расчет под номером 1 '
-                                    'для ввода данных или печати отчета \n',
-                  italic('/delall'), ' - Удалить все расчета \n',
-                  # italic('/detail1'), ' - Детальная информация о расчете номер 1 \n',
-                  # italic('/delreceipt4'), ' - Удалить чек номер 4 из активного расчета \n',
+                  '_/new_', ' - Новый расчет \n',
+                  '_Имя1 300 Имя2 Имя3_', ' - Формат ввода данных чека. Имя1 - оплатил чек, '
+                                          '300 - сумма чека, Имя2 и Имя3 - должники  \n',
+                  '_/report_', ' - Вывод текущего расчета \n',
+                  '_/picreport_', ' - Вывод текущего расчета в виде  \n',
+                  '_/report Имя1 Имя2_', ' - Вывод детали расчета. '
+                                         'Можно узнать сколько Имя2 должен Имя2 \n',
+                  '_/all_', ' - Вывод всех расчетов \n',
+                  '_/del1_', ' - Удаление расчета под номером 1 \n',
+                  '_/calc1_', ' - Сделать активным расчет под номером 1 '
+                              'для ввода данных или печати отчета \n',
+                  '_/delall_', ' - Удалить все расчета \n',
+                  # '_/detail1_', ' - Детальная информация о расчете номер 1 \n',
+                  # '_/delreceipt4_', ' - Удалить чек номер 4 из активного расчета \n',
                   '', )
 
 
@@ -33,7 +33,7 @@ def text_for_report(calc_id, calc_alias, report_data,
 
 
 def text_all_calcs(all_calcs):
-    answer = bold('Список всех расчетов\n')
+    answer = '(Список всех расчетов\n*'
     for calc in all_calcs:
         calc_alias = calc.calc_alias
         calc_id = calc.calc_id
@@ -46,21 +46,19 @@ def text_all_calcs(all_calcs):
 
 
 def text_all_receipts(all_receipts):
-
     calc_id = all_receipts[0].calc_id
     calc_alias = all_receipts[0].calc_alias
 
     calc_alias = calc_alias if calc_alias else '(Без имени)'
-    answer = bold(f'Список всех чеков по расчету номер {calc_id} с именем {calc_alias} \n')
+    answer = f'*Список всех чеков по расчету номер {calc_id} с именем {calc_alias} \n*'
     for receipt in all_receipts:
-
         receipt_num = receipt.receipt_id
         sponsor = receipt.sponsor
         receipt_money = receipt.sum
         consumers = receipt.consumers
 
         consumers = [consumer for consumer in consumers if consumer is not None]
-        answer += (bold(f'{receipt_num}: ') +
-                   bold(f'{sponsor}') + ' оплатил ' + f'_{receipt_money}_ ' +
-                   f'за {consumers}\\. ' + f'Удалить чек /receiptdel{receipt_num} \n')
+        answer += (f'*{receipt_num}:* ' +
+                   f'*{sponsor}*' + ' оплатил ' + f'_{receipt_money}_ ' +
+                   f'за {consumers}. ' + f'Удалить чек /receiptdel{receipt_num} \n')
     return text(answer)
