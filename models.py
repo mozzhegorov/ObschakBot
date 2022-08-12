cec53dfb7b40e9bdde0486233402be06d7b75f0f
denis <denislegrace@yandex.ru>
2022-08-12 13:06:46 +0500

    refactoring

--- models.py
+++ models.py
@@ -83,7 +83,7 @@ class Receipt(DeclarativeBase):
     consumers = relationship(
         "Person",
         secondary=persons_in_receipts,
-        # cascade="all, delete",
+        cascade="delete",
         # backref=backref("persons", uselist=False),
     )
 
