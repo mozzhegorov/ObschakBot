e2a8f944d800ff0bf182946bc74d7cea69331e6f
denis <denislegrace@yandex.ru>
2022-08-12 13:16:15 +0500

    refactoring

--- services.py
+++ services.py
@@ -142,8 +142,8 @@ def delete_calc(user_id: int, calc_id: int):
         new_active_calc = get_last_calc_by_user(user_id, session)
         new_active_calc.active = True
     session.query(Receipt).\
-        where(Receipt.calc_id == calc_id). \
-        where(Receipt.user_id == user_id).delete()
+        filter(Receipt.calc_id == calc_id). \
+        filter(Receipt.user_id == user_id).delete()
     # if receipt_for_delete:
     #     session.delete(receipt_for_delete)
     session.commit()
