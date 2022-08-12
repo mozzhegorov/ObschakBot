887107794a63a1017648c2ce3c4e4a592043705f
denis <denislegrace@yandex.ru>
2022-08-12 09:15:20 +0500

    refactoring

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, Table, Float, Boolean, select
+from sqlalchemy import ForeignKey, Column, Integer, Table, Float, Boolean, select, BigInteger
 from sqlalchemy import String
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
